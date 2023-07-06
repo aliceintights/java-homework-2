0cda2a5320b4bd9ffe75a81be602663e701382cd
irina <obesnyuk@gmail.com>
2023-07-07 01:28:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 9458;
+        int bonusmile = 20;
+
+        System.out.println(ticket / bonusmile);
+
+        // int endbonus = 472;
+    }
+}
